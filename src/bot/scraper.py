dbba51710a7fb7897e089e4ed85e364fdfb6fc38
httpsdinha <amandabalcacar@gmail.com>
2025-04-24 12:30:28 -0400

    feat(scraper): estrutura basica da função de scraping

---
+++ src/bot/scraper.py
@@ -0,0 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_cs_updates():
+    """Busca tweets da FURIA sobre CS no Nitter"""
+    pass
---
+++ src/bot/test_scraper.py
@@ -0,0 +1,2 @@
+from src.bot.scraper import get_cs_updates
+print(get_cs_updates())
